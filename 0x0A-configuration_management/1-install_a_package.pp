275c0c105a4a67c7a8f031ed39137cac0abae6f4
AskourTaoufyq <kosa477kacm@gmail.com>
2024-06-30 14:20:27 -0700

    0x0A-configuration_management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,6 @@
-#!/usr/bin/pup
-# Install flask version (2.1.0)
-
-package {'flask':
+# To install Flask version 2.1.0 using Puppet
+package { 'Flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip3',
 }
+
